9712b23bc03c7c97a1cabc02d18cfe469939bdf5
Maurizio Vitale <mauriziovit@gmail.com>
2016-10-02 18:37:23 -0400

    rename ctx

--- csp.py
+++ csp.py
@@ -4,10 +4,10 @@ import sys, math
 #Local Files
 import Classes as cs
 
-def checkFinalState(ctx):                                         #Checks whether this is valid for the final state.
-    for i in ctx['items']:                                        #Since all of the data is in the items, let's loop through that.
-        item = ctx['items'][i];                                   #For shorthand purposes
-        b = ctx['bags'][ctx['items'][i].currentBag]               #For shorthand purposes
+def checkFinalState(csp):                                         #Checks whether this is valid for the final state.
+    for i in csp['items']:                                        #Since all of the data is in the items, let's loop through that.
+        item = csp['items'][i];                                   #For shorthand purposes
+        b = csp['bags'][csp['items'][i].currentBag]               #For shorthand purposes
 
         if b.name not in item.unary_inclusive:                    #Check if the object is in a bag it isn't technically allowed in
             return False
@@ -33,85 +33,83 @@ def checkFinalState(ctx):                                         #Checks whethe
             if it in b.items:                                               #If it's in the current bag, return false.
                 return False
             else:
-                if ctx['items'][it].currentBag in mutual_inclusivity_bag_copy:          #If the bag is in the list, remove the first instance.
-                    mutual_inclusivity_bag_copy.remove(ctx['items'][it].currentBag)
+                if csp['items'][it].currentBag in mutual_inclusivity_bag_copy:          #If the bag is in the list, remove the first instance.
+                    mutual_inclusivity_bag_copy.remove(csp['items'][it].currentBag)
                 else:                                                                   #Otherwise, mutual inclusivity failed.
                     return False
 
-    for b in ctx['bags']:                                          #Loop through the bags to make sure they're all ok.
-        if not ctx['bags'][b].validBag():                          #Check them.
+    for b in csp['bags']:                                          #Loop through the bags to make sure they're all ok.
+        if not csp['bags'][b].validBag():                          #Check them.
             return False
 
     return True
 
-
-
-def readargs(ctx, cursor, args):
+def readargs(csp, cursor, args):
     if args[0] == '#####':
         return cursor + 1
 
     if (cursor == 1):
-        readvariables(ctx, args)
+        readvariables(csp, args)
     elif (cursor == 2):
-        readvalues(ctx, args)
+        readvalues(csp, args)
     elif (cursor == 3):
-        readlimits(ctx, args)
+        readlimits(csp, args)
     elif (cursor == 4):
-        readunaryinc(ctx, args)
+        readunaryinc(csp, args)
     elif (cursor == 5):
-        readunaryex(ctx, args)
+        readunaryex(csp, args)
     elif (cursor == 6):
-        readbinaryeq(ctx, args)
+        readbinaryeq(csp, args)
     elif (cursor == 7):
-        readbinarynoteq(ctx, args)
+        readbinarynoteq(csp, args)
     elif (cursor == 8):
-        readmutual(ctx, args)
+        readmutual(csp, args)
 
     return cursor
 
-def readbinaryeq(ctx, args):
+def readbinaryeq(csp, args):
     for arg in args:
         for a in args:
             if a is not arg:
-                ctx['items'][arg].addEquality(a)
+                csp['items'][arg].addEquality(a)
 
-def readbinarynoteq(ctx, args):
+def readbinarynoteq(csp, args):
     for arg in args:
         for a in args:
             if a is not arg:
-                ctx['items'][arg].addInequality(a)
+                csp['items'][arg].addInequality(a)
 
-def readmutual(ctx, args):
+def readmutual(csp, args):
     for arg in args:
         for a in args:
             if a is not arg and arg.isupper():
                 if a.isupper():
-                    ctx['items'][arg].addMutualIncItem(a)
+                    csp['items'][arg].addMutualIncItem(a)
                 else:
-                    ctx['items'][arg].addMutualIncBag(a)
+                    csp['items'][arg].addMutualIncBag(a)
 
 
-def readunaryex(ctx, args):
+def readunaryex(csp, args):
     for arg in args[1:]:
-        ctx['items'][args[0]].addUnaryEx(arg)
+        csp['items'][args[0]].addUnaryEx(arg)
 
-def readunaryinc(ctx, args):
+def readunaryinc(csp, args):
     for arg in args[1:]:
-        ctx['items'][args[0]].addUnaryIn(arg)
+        csp['items'][args[0]].addUnaryIn(arg)
 
-def readlimits(ctx, args):
-    for b in ctx['bags']:
-        ctx['bags'][b].setMinMax(int(args[0]), int(args[1]))
+def readlimits(csp, args):
+    for b in csp['bags']:
+        csp['bags'][b].setMinMax(int(args[0]), int(args[1]))
 
-def readvariables(ctx, args):
-    ctx['items'][args[0]] = cs.Item(args[0], int(args[1]))
+def readvariables(csp, args):
+    csp['items'][args[0]] = cs.Item(args[0], int(args[1]))
 
-def readvalues(ctx, args):
-    ctx['bags'][args[0]] = cs.Bag(args[0], int(args[1]))
+def readvalues(csp, args):
+    csp['bags'][args[0]] = cs.Bag(args[0], int(args[1]))
 
 def main(argv):
 
-    ctx = {
+    csp = {
         'bags': {},
         'items': {},
     }
@@ -122,33 +120,33 @@ def main(argv):
 
         for line in file:
             args = line.strip().split()
-            cursor = readargs(ctx, cursor, args)
+            cursor = readargs(csp, cursor, args)
 
 
-    if ctx['bags']['b'].addToBag(ctx['items']['G']) is -1:
+    if csp['bags']['b'].addToBag(csp['items']['G']) is -1:
         print()
         print("G could not be added to b")
 
-    if ctx['bags']['b'].addToBag(ctx['items']['E']) is -1:
+    if csp['bags']['b'].addToBag(csp['items']['E']) is -1:
         print()
         print("E could not be added to b")
 
-    if ctx['bags']['c'].addToBag(ctx['items']['G']) is -1:
+    if csp['bags']['c'].addToBag(csp['items']['G']) is -1:
         print()
         print("G could not be added to c")
 
-    if ctx['bags']['a'].addToBag(ctx['items']['C']) is -1:
+    if csp['bags']['a'].addToBag(csp['items']['C']) is -1:
         print()
         print("C could not be added to a")
 
     print()
 
-    for b in ctx['bags']:
-        ctx['bags'][b].printBag()
+    for b in csp['bags']:
+        csp['bags'][b].printBag()
         print()
 
-    for i in ctx['items']:
-        ctx['items'][i].printItem()
+    for i in csp['items']:
+        csp['items'][i].printItem()
         print()
 
 if __name__ == "__main__":
